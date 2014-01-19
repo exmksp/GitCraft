a0c58945f6b6667421d10c977219ae8b7ba9ad67
exm <exmksp@yahoo.com>
2014-01-19 01:26:03 -0600

    refactoring (major)

--- source/GitCraft/GitModule.cs
+++ source/GitCraft/GitModule.cs
@@ -15,7 +15,7 @@ namespace GitNS
                 return;
             if (null == GitPartless.fetch)
                 return;
-            GitPartless.fetch.needsCommit = true;
+            GitPartless.fetch.shipModified = true;
             node.ClearData();
         }
     }
@@ -23,12 +23,11 @@ namespace GitNS
     [KSPAddon(KSPAddon.Startup.EditorAny, false)]
     public class GitPartless : MonoBehaviour
     {
+        public bool shipModified = false;
         private static Texture2D gitLogo = new Texture2D(80, 34, TextureFormat.ARGB32, false);
-        public static string DirSep = System.IO.Path.DirectorySeparatorChar.ToString();
         public static GitPartless fetch = null;
 
-        public static ShipConstruct partfulShipBeingEdited()
-        {
+        public static ShipConstruct PartfulShipBeingEdited() {
             // This also prevents partless ships from displaying the git UI.
             if (!HighLogic.LoadedSceneIsEditor || null == EditorLogic.fetch || null == EditorLogic.fetch.ship)
                 return null;
@@ -39,37 +38,120 @@ namespace GitNS
                 return ship;
         }
 
+        // Returns the name of the ship currently being edited.
+        private static string ShipName() {
+            if (ShipConstruction.ShipConfig == null)
+                return null;
+            else
+                return ShipConstruction.ShipConfig.GetValue("ship");
+        }
+
+        // Return the filename of the ship currently being edited.
+        public static string ShipFilename() {
+            string shipName = ShipName();
+            if (null == shipName)
+                return null;
+            return  KSPUtil.SanitizeString(shipName, '_', true) + ".craft";
+        }
+
+        // Get the full path to the directory where the current game is saved.
+        private static string CurrentGameDirectory() {
+            if (null == HighLogic.SaveFolder)
+                return null;
+            else
+                return string.Concat(new string[] { KSPUtil.ApplicationRootPath, "saves",
+                    Path.DirectorySeparatorChar.ToString(), HighLogic.SaveFolder });
+        }
+
+        // Get path to a ship relative to a repository, which is in the current game directory.
+        private static string RelativeShipPath() {
+            string filename = ShipFilename();
+            if (filename == null)
+                return null;
+            string subFolder = ShipConstruction.GetShipsSubfolderFor(HighLogic.LoadedScene);
+            if (subFolder == null)
+                return null;
+            return string.Concat(new string[] { "Ships", Path.DirectorySeparatorChar.ToString(),
+                subFolder, Path.DirectorySeparatorChar.ToString(), filename });
+        }
+
+        public static string FullShipPath() {
+            string gamedir = CurrentGameDirectory();
+            if (null == gamedir)
+                return null;
+            string relpath = RelativeShipPath();
+            if (null == relpath)
+                return null;
+            return string.Concat(new string[] { gamedir, Path.DirectorySeparatorChar.ToString(), relpath });
+        }
+
+        // See if there is a .git directory in the current game directory. In good faith this assumes
+        // if the directory exists, then it is fully set up and ready for action.
+        private static bool HaveDotGitDirSetUp()
+        {
+            string gamedir = CurrentGameDirectory();
+            if (null == gamedir)
+                return false;
+            else
+                return Directory.Exists(gamedir + Path.DirectorySeparatorChar + ".git");
+        }
+
+        // Get the current repository status of the ship.
+        private static FileStatus? ShipRepoStatus() {
+            if (!HaveDotGitDirSetUp())
+                return null;
+            string relpath = RelativeShipPath();
+            return ObtainRepository().Index.RetrieveStatus(relpath);
+        }
+
+        // This is what should be used to obtain an instance of Repository. The code creates
+        // an empty repository, if it does not exist.
+        private static Repository ObtainRepository() {
+            string gamedir = CurrentGameDirectory();
+            if (null == gamedir)
+                return null;
+            if (!HaveDotGitDirSetUp()) {
+                LibGit2Sharp.Repository.Init(gamedir);
+            }
+            return new Repository(gamedir);
+        }
+
         void Update()
         {
-            ShipConstruct ship = partfulShipBeingEdited();
+            ShipConstruct ship = PartfulShipBeingEdited();
             if (null == ship)
                 return;
             List<Part> parts = ship.Parts;
-            if (needsCommit) {
-                // This is a bit paranoid, but it is necessary to make .craft files look as if there was
-                // never an injection of the GitModule. As of 0.23, things work just fine even without this,
-                // but who knows. It would suck if users get into trouble because of some empty MODULE
-                // sections. This doesn't really work during the launch, but I think that that should be fine.
-                // The "Auto Saved Ship.craft" files will have references to GitModule and there isn't much
-                // that I can do about them apart from making them empty or implementing some elaborate recovery
-                // strategy which is not worth it because who would share an auto saved ship anyway?
+            if (shipModified && File.Exists(FullShipPath())) {
+                FileStatus? status = ShipRepoStatus();
+                // This code may be triggered from GitModule.OnSave() that were not really about saving.
+                // To filter correct triggers, we need to check that there is a ship and that it has actually changed.
+                if (status != null && status.Value != FileStatus.Unaltered) {
+                    // This is a bit paranoid, but it is necessary to make .craft files look as if there was
+                    // never an injection of the GitModule. As of 0.23, things work just fine even without this,
+                    // but who knows. It would suck if users get into trouble because of some empty MODULE
+                    // sections. This doesn't really work during the launch, but I think that that should be fine.
+                    // The "Auto Saved Ship.craft" files will have references to GitModule and there isn't much
+                    // that I can do about them apart from making them empty or implementing some elaborate recovery
+                    // strategy which is not worth it because who would share an auto saved ship anyway?
 
-                // Hopefully, all of this is useless, because SQUAD should be very defensive with .craft loading code 
-                // now that there are so many plugins floating around.
-                for (int ix = 0; ix < parts.Count; ++ix)
-                    if (null != parts[ix] && null != parts[ix].Modules)
-                    if (parts[ix].Modules.Contains("GitModule")) {
-                        for (int kx = 0; kx < parts[ix].Modules.Count; ++kx)
-                            if (parts[ix].Modules[kx] is GitModule) {
-                                Debug.Log("Pulling GitModule out!");
-                                //parts[ix].Modules.Remove(parts[ix].Modules[kx]);
-                                parts[ix].RemoveModule(parts[ix].Modules[kx]);
-                                break;
-                            }
-                    }
-                Debug.Log("re-saving and committing");
-                ShipConstruction.SaveShip(ship, ship.shipName); // get rid of the MODULE node, just in case
-                commitAsNeeded();
+                    // Hopefully, all of this is useless, because SQUAD should be very defensive with .craft loading code 
+                    // now that there are so many plugins floating around.
+                    for (int ix = 0; ix < parts.Count; ++ix)
+                        if (null != parts[ix] && null != parts[ix].Modules)
+                        if (parts[ix].Modules.Contains("GitModule")) {
+                            for (int kx = 0; kx < parts[ix].Modules.Count; ++kx)
+                                if (parts[ix].Modules[kx] is GitModule) {
+                                    Debug.Log("Pulling GitModule out!");
+                                    //parts[ix].Modules.Remove(parts[ix].Modules[kx]);
+                                    parts[ix].RemoveModule(parts[ix].Modules[kx]);
+                                    break;
+                                }
+                        }
+                    Debug.Log("re-saving and committing");
+                    ShipConstruction.SaveShip(ship, ship.shipName); // get rid of the MODULE node, just in case
+                    commitAsNeeded();
+                }
             } 
             // Now that we are partless, we need to attach ourselves somewhere so that we
             // can intercept onSave(). We check the first part of the ship in each frame and attach
@@ -95,65 +177,11 @@ namespace GitNS
             fetch = this;
         }
 
-        private static string ShipName()
-        {
-            if (ShipConstruction.ShipConfig == null)
-                return null;
-            else
-                return ShipConstruction.ShipConfig.GetValue("ship");
-        }
-        // Get the full path to the directory where the saved games are.
-        private static string getSaveFolder()
-        {
-            if (null == HighLogic.SaveFolder)
-                return null;
-            else
-                return string.Concat(new string[] {
-                    KSPUtil.ApplicationRootPath, "saves", DirSep,
-                    HighLogic.SaveFolder
-                });
-        }
-        // Get full path to a ship.
-        private static string fullShipPath(string shipFileName)
-        {
-            if (shipFileName == null)
-                return null;
-            string saveFolder = getSaveFolder();
-            if (saveFolder == null)
-                return null;
-            string relPath = RelShipPath(shipFileName);
-            if (relPath == null)
-                return null;
-            return string.Concat(new string[] { saveFolder, DirSep, relPath });
-        }
-        // Get path to a ship relative to a repository.
-        private static string RelShipPath(string shipFileName)
-        {
-            if (shipFileName == null)
-                return null;
-            string subFolder = ShipConstruction.GetShipsSubfolderFor(HighLogic.LoadedScene);
-            if (subFolder == null)
-                return null;
-            return string.Concat(new string[] { "Ships", DirSep, subFolder,
-                DirSep, shipFileName
-            });
-        }
-        // See if we have the git repository set up.
-        private static bool haveGitRepo()
-        {
-            string saveFolder = getSaveFolder();
-            if (null == saveFolder)
-                return false;
-            else
-                return Directory.Exists(saveFolder + System.IO.Path.DirectorySeparatorChar + ".git");
-        }
-
         private Signature getSignature()
         {
             return new Signature("GitCraft Plugin for KSP", "gitcraft@localhost", System.DateTimeOffset.Now);
         }
 
-        public bool needsCommit = false;
         // Timestamp for the craft in game time, earth units.
         public static string commitTS()
         {
@@ -183,35 +211,21 @@ namespace GitNS
                 return null;
         }
 
-        public static string ShipFileName()
-        {
-            return  KSPUtil.SanitizeString(ShipName(), '_', true) + ".craft";
-        }
-
         public void commitAsNeeded()
         {
-            needsCommit = false;
-            string shipFileName = ShipFileName();
-            print("Ship type: " + ShipConstruction.ShipType);
-            string fullPath = fullShipPath(shipFileName);
-            print("Full ship path: " + (fullPath == null ? "<null>" : fullPath));
-            if (null == fullPath)
+            shipModified = false;
+            string fullpath = FullShipPath();
+            if (null == fullpath)
                 return;
-            if (!haveGitRepo() && null != getSaveFolder()) {
-                print("Initializng the repository...");
-                LibGit2Sharp.Repository.Init(getSaveFolder());
-                print("Done.");
-            }
-            if (haveGitRepo()) {
+            if (HaveDotGitDirSetUp()) {
                 // commit if necessary
-                string relPath = RelShipPath(shipFileName);
-                print(String.Format("Relative path = {0}", relPath));
-                Repository repo = new Repository(getSaveFolder());
-                FileStatus status = repo.Index.RetrieveStatus(relPath);
+                string relpath = RelativeShipPath();
+                FileStatus? status = ShipRepoStatus();
                 print(String.Format("Repository status = {0}", status.ToString()));
-                if (FileStatus.Unaltered != status && FileStatus.Nonexistent != status) {
-                    print(String.Format("Committing craft {0}", relPath));
-                    repo.Index.Stage(relPath);
+                if (null != status && FileStatus.Unaltered != status.Value && FileStatus.Nonexistent != status.Value) {
+                    Repository repo = ObtainRepository();
+                    print(String.Format("Committing craft {0}", relpath));
+                    repo.Index.Stage(relpath);
                     string msg = ""; // + relPath + "\", ";
                     int? np = numParts();
                     if (np != null)
@@ -323,7 +337,7 @@ namespace GitNS
                         GUI.skin.button.stretchWidth = true;
                         if (GUILayout.Button(idarr[ix].Substring(0, prefL))) {
                             Checkout(idarr[ix]);
-                            EditorLogic.LoadShipFromFile(fullShipPath(ShipFileName()));
+                            EditorLogic.LoadShipFromFile(FullShipPath());
                         }
                         GUILayout.Label(hist[ix].Message);
                         GUILayout.FlexibleSpace();
@@ -345,7 +359,7 @@ namespace GitNS
 
         public void drawGUI()
         {
-            if (null == partfulShipBeingEdited())
+            if (null == PartfulShipBeingEdited())
                 return;
             GUIStyle winst = new GUIStyle();
             winst.border.left = winst.border.top = winst.border.right = winst.border.bottom = 0;
@@ -371,11 +385,8 @@ namespace GitNS
         {
             List<Commit> list = new List<Commit>();
             HashSet<string> dedup = new HashSet<string>();
-            string saveFolder = getSaveFolder();
-            string relPath = RelShipPath(ShipFileName());
-            if (null == saveFolder || null == relPath)
-                return list;
-            Repository repo = new Repository(saveFolder);
+            string relPath = RelativeShipPath();
+            Repository repo = ObtainRepository();
             CommitFilter cf = new CommitFilter();
             // get commits in reverse topological order so that parents go first
             // this works together with deduplicator to obtain something like git-log
@@ -396,9 +407,11 @@ namespace GitNS
         // Checkout the current ship from a specified commit.
         public void Checkout(string id)
         {
-            Repository repo = new Repository(getSaveFolder());
-            string relPath = RelShipPath(ShipFileName());
-            repo.CheckoutPaths(id, new string[] { relPath });
+            Repository repo = new Repository(CurrentGameDirectory());
+            string relPath = RelativeShipPath();
+            CheckoutOptions opts = new CheckoutOptions();
+            opts.CheckoutModifiers = CheckoutModifiers.Force;
+            repo.CheckoutPaths(id, new string[] { relPath }, opts);
             print("Checked out " + relPath + " from " + id);
         }
     }
